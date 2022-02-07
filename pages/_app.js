10441dea6c0541b7803482fd6623611e7152543c
Shae Allison <sallison@adkgroup.com>
2022-02-07 17:15:36 -0500

    working next example from basics tutorial

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/global.css'
+import utilStyles from '../styles/utils.module.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
